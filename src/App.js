d42f942c9787de55d62349679d736480c0a24188
unknown <alessandro.grunwald@gmail.com>
2018-02-06 17:46:59 -0800

    a

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
     render() {
         if(this.state.tab ==1){
             return (
-                <div className="App">
+                <div className="App" >
                     <button onClick={this.openChat} >Chat</button>
                 
                     <p id="info1" style={{transition: "all 1s ease-out", opacity: this.state.infoOpacity, display: this.state.infoDisplay}}>
@@ -118,7 +118,7 @@ class App extends Component {
                 </div>
             );
         }else if(this.state.tab == 2){
-            return (
+            return ( 
                 <Chat />
             );   
         }
