67995319a6735bdbe74fa41379d58394165c869b
SandroBCIT <alessandro.grunwald@gmail.com>
2018-03-19 22:49:54 -0700

    added click and color props

--- src/coloring/Coloring.js
+++ src/coloring/Coloring.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import Golf from './svg/Golf.js';
 import '../App.css';
-//import './index.css';
 
 class Coloring extends React.Component {
     constructor() {
         super();
         this.state = {
-//            endpoint: "http://127.0.0.1:4001",
-//            color: "black"
-            frontDoorColor: "white"
+            frontDoorColor: "white",
+            mainFrameColor: "white",
+            frontDoorColor: "white",
+            rearDoorColor: "white",
+            rearIndicatorColor: "white",
+            sideIndicatorColor: "white",
+            rearHandleColor: "white",
+            frontHandleColor: "white",
+            something3Color: "white",
+            rearTireShadowColor: "white",
+            rearHubColor: "white",
+            rearTireColor: "white",
+            frontTireColor: "white",
+            frontHubColor: "white",
+            frontLogoColor: "black",
+            rearWindowColor: "white",
+            windowTrimColor: "white",
+            rearLogoColor: "black"
         };
     }
-
-//    send = () => {
-//        const socket = socketIOClient(this.state.endpoint); 
-//        socket.emit('change color', this.state.color);
-//        console.log("send it");
-//    }
-//
-//    setColor = (color) => {
-//        this.setState({ color });
-//        console.log(color);   
-//    }]
     
     frontDoorClick = ()=>{
         this.setState({
@@ -30,6 +33,107 @@ class Coloring extends React.Component {
         })
     }
 
+    mainFrameClick = ()=>{
+        this.setState({
+            mainFrameColor: "green"
+        })  
+    }
+    
+    frontDoorClick = ()=>{
+        this.setState({
+            frontDoorColor: "green"
+        })
+    }
+    
+    rearDoorClick = ()=>{
+        this.setState({
+            rearDoorColor: "green"
+        })
+    }
+    
+    rearIndicatorClick = ()=>{
+        this.setState({
+            rearIndicatorColor: "green"
+        })
+    }
+    
+    sideIndicatorClick = ()=>{
+        this.setState({
+            sideIndicatorColor: "green"
+        }) 
+    }
+    
+    rearHandleClick = ()=>{
+        this.setState({
+            rearHandleColor: "green"
+        })
+    }
+    
+    frontHandleClick = ()=>{
+        this.setState({
+            frontHandleColor: "green"
+        }) 
+    }
+    
+    something3Click = ()=>{
+        this.setState({
+            something3Color: "green"
+        }) 
+    }
+    
+    rearTireShadowClick = ()=>{
+        this.setState({
+            rearTireShadowColor: "green"
+        })
+    }
+    
+    rearHubClick = ()=>{
+        this.setState({
+            rearHubColor: "green"
+        })
+    }
+    
+    rearTireClick = ()=>{
+        this.setState({
+            rearTireColor: "green"
+        })
+    }
+    
+    frontTireClick = ()=>{
+        this.setState({
+            frontTireColor: "green"
+        })
+    }
+    
+    frontHubClick = ()=>{
+        this.setState({
+            frontHubColor: "green"
+        })
+    }
+    
+    frontLogoClick = ()=>{
+        this.setState({
+            frontLogoColor: "green"
+        })
+    }
+    
+    rearWindowClick = ()=>{
+        this.setState({
+            rearWindowColor: "green"
+        })
+    }
+    
+    windowTrimClick = ()=>{
+        this.setState({
+            windowTrimColor: "green"
+        })
+    }
+    
+    rearLogoClick = ()=>{
+        this.setState({
+            rearLogoColor: "green"
+        }) 
+    }
 
     render() {
         return (
@@ -37,6 +141,59 @@ class Coloring extends React.Component {
                 <Golf 
                     frontDoorColor={this.state.frontDoorColor}
                     frontDoorClick={this.frontDoorClick}
+            
+                    mainFrameClick = {this.mainFrameClick}
+                    mainFrameColor = {this.state.mainFrameColor}
+
+                    frontDoorClick = {this.frontDoorClick}
+                    frontDoorColor = {this.state.frontDoorColor}
+
+                    rearDoorClick = {this.rearDoorClick}
+                    rearDoorColor = {this.state.rearDoorColor}
+
+                    rearIndicatorClick = {this.rearIndicatorClick}
+                    rearIndicatorColor = {this.state.rearIndicatorColor}
+
+                    sideIndicatorClick = {this.sideIndicatorClick}
+                    sideIndicatorColor = {this.state.sideIndicatorColor}
+
+                    rearHandleClick = {this.rearHandleClick}
+                    rearHandleColor = {this.state.rearHandleColor}
+
+                    frontHandleClick = {this.frontHandleClick}
+                    frontHandleColor = {this.state.frontHandleColor}
+
+                    something3Click = {this.something3Click}
+                    something3Color = {this.state.something3Color}
+
+                    rearTireShadowClick = {this.rearTireShadowClick}
+                    rearTireShadowColor = {this.state.rearTireShadowColor}
+
+                    rearHubClick = {this.rearHubClick}
+                    rearHubColor = {this.state.rearHubColor}
+
+                    rearTireClick = {this.rearTireClick}
+                    rearTireColor = {this.state.rearTireColor}
+
+                    frontTireClick = {this.frontTireClick}
+                    frontTireColor = {this.state.frontTireColor}
+
+                    frontHubClick = {this.frontHubClick}
+                    frontHubColor = {this.state.frontHubColor}
+
+                    frontLogoClick = {this.frontLogoClick}
+                    frontLogoColor = {this.state.frontLogoColor}
+
+                    rearWindowClick = {this.rearWindowClick}
+                    rearWindowColor = {this.state.rearWindowColor}
+
+                    windowTrimClick = {this.windowTrimClick}
+                    windowTrimColor = {this.state.windowTrimColor}
+            
+                    rearLogoClick = {this.rearLogoClick}
+                    rearLogoColor = {this.state.rearLogoColor}
+            
+                
                 />
             </div>
         )
